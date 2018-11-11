ee858fd8103bfe23bd7bbd6f948ac6fc2e94edbc
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-11 20:36:27 +0100

    first commit

---
+++ html/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<?php
+			if (!empty($argv[2])) 
+				echo '<link rel="stylesheet" href="css/'.$argv[2].'">';
+		?>
+		<title>Titre de page</title>
+	</head>
+	<body>
+		<?php
+			include($argv[1]);
+		?>
+	</body>
+</html>
